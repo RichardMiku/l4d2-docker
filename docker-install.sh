062fae389a2e3bbf0062bd00ceafc588a0c287b4
Richard Miku <richardmiku@vip.qq.com>
2025-01-20 10:27:48 +0800

    Fix docker-install script by removing unnecessary dollar sign from install command

--- docker-install.sh
+++ docker-install.sh
@@ -1,3 +1,3 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-$ sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io
